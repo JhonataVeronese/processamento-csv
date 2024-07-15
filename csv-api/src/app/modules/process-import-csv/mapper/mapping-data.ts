63fec963b3bd973e5b83f92f877e5393c5aea749
Jhonata Veronese <jhonataveronese@gmail.com>
2024-07-15 17:04:12 -0300

    refactor: Ajuste para nova top do desafio

--- csv-api/src/app/modules/process-import-csv/mapper/mapping-data.ts
+++ csv-api/src/app/modules/process-import-csv/mapper/mapping-data.ts
@@ -19,26 +19,6 @@ export class MapperResult {
     let situacaoParcela = "Pendente";
     if (parcelValueInvalid) {
       situacaoParcela = "Parcela com inconsistência";
-      // Aqui entraria a logica para tratativa do ponto do desafio a baixo, porém fiquei em duvida sobre alguns dados e
-      // informações faltantes, para ser mais preciso na solução e fiz o questionamento para o Alexandre Pegoro
-
-      // Ponto que gerou a duvida: -----------------
-      /* Para chegar a um valor aproximado, devera converter o valor total para um numero inteiro, ,
-      ignorando as dezenas quebradas, e calculando a data de pagamento para verificar se existe juros 
-      acumulado, e o valor de mora, caso o valor do movimento(vlMovimento) seja maior que o valor  do pagamento (vlPag), 
-      devera construir uma trataviva adequada dizendo que o pagamento está inconsistente. */
-
-      /************
-       ****** Duvida enviada para o Alexandre
-       * O ultimo ponto do desafio fala sobre chegar no valor aproximado ( acredito que seria o valor da parcela corrigido) porém após ler
-       * os dados alguns campos que são informados no desafio não existem no arquivo (vlMovimento e vlPag ) além de não conter a informação de data de pagamento>
-       * Outro ponto são as informações de datas ( dtContrato e dtVctPre ) Eu até consigo calcular a diferença em dias entre essas datas pra tentar chegar a um valor de
-       * uros diarios descontando dai o valor de multa e mora pra saber o valor de juros no periodo, porém analisando as datas, não fica claro se a data de contrato seria
-       *  o prazo final considerando todas as parcelas ou se precioso considerar a parcela atual ( essa informação existe no arquivo ) Porque existe uma diferença nas datas em si que gerou essa duvida.
-       * Eu cheguei até esse ponto, todo o restantes já está implementado estou apenas organizando a rota para que seja possivel realizar testes e ter os resultados apresentados.
-       * Mas acredito ser melhor tirar essas duvidas enquanto o requisito está em desenolvimento do que implementar algo com duvida e precisar de um refactor posteriormente.
-       *************
-       */
     }
 
     return {
